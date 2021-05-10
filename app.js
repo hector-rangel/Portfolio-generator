713ef2763318b78c772fe584078f32bd69c911f3
Hector Rangel <hector.rangeljr@yahoo.com>
2021-05-10 16:08:14 -0500

    added app.js/page-template.js and created html

---
+++ app.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+// // notice the lack of parentheses around the 'profileDataArr' paramater?
+// const printProfileData = profileDataArr => {
+//     // this...
+//     for (let i=0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('============');
+
+//     // is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name,github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
