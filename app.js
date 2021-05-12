505ee6ac16b65a09abb422177da1d5f205d0ff1c
Hector Rangel <hector.rangeljr@yahoo.com>
2021-05-11 20:57:56 -0500

    added inquirer package and gitignore

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name,github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-// // notice the lack of parentheses around the 'profileDataArr' paramater?
-// const printProfileData = profileDataArr => {
-//     // this...
-//     for (let i=0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('============');
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
-//     // is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-fs.writeFile('./index.html', generatePage(name,github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+inquirer 
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
