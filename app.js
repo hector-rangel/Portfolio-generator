8754fd477bbf5e7ecabf904f274bc59d9f52859b
Hector Rangel <hector.rangeljr@yahoo.com>
2021-05-12 11:40:03 -0500

    added if statements for certain questions to provide a answer

--- app.js
+++ app.js
@@ -23,12 +23,30 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: (projectName) => {
+                if (projectName) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter project name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: (projectDescription) => {
+                if (projectDescription) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -39,7 +57,16 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: (projectLink) => {
+                if (projectLink) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter a github link');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
@@ -69,12 +96,30 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your Github Username'
+            message: 'Enter your Github Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your github!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
